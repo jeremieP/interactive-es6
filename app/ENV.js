8e2f744fec091b1bfbe3553dd026c35b2a7f8c8a
Jeremie Pereira <jeremie.pereira@schibsted.com>
2018-02-12 10:03:33 +0100

    Update pusher keys

--- app/ENV.js
+++ app/ENV.js
@@ -1,2 +1,2 @@
-export const PUSHER_APP_KEY = '750df8540776dad8f3bf';
-export const PUSHER_APP_SECRET = '3fa0d9d2de75b7c10b76';
+export const PUSHER_APP_KEY = '1d9e1b337279eea79c43';
+export const PUSHER_APP_SECRET = '65a9674e301bc602fa7b';
